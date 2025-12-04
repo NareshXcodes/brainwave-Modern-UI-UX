19a8157478874c4b045c187c8692835786bf25fa
Naresh Mahapatra <codes.xdev@gmail.com>
2025-12-04 18:21:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Button from './components/Button.jsx';
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
+
+
+
+function App() {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+}
+
+export default App;
